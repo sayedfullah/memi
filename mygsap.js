3655e612a41b5c0a3b508295cb57742a6d702154
sayedfullah <42635601+sayedfullah@users.noreply.github.com>
2018-08-24 12:29:14 +0200

    Update mygsap.js

--- mygsap.js
+++ mygsap.js
@@ -18,7 +18,7 @@ myScene
     .to(".start",1,{x:"8%",ease:Linear.easeOut,zIndex:18},"-=0.5")
     .to(".end",1,{x:"-6.5%",ease:Linear.easeOut},"-=0.5")
     //swap out start chain
-    .to(".dual",0.1,{src:"https://s8.postimg.cc/esh0uemv9/chain_Dual.png",ease:Linear.easeIn},"-=0.1")
+    .to(".dual",0.1,{src:"/design/themes/GiddyStore/media/animation/01/res/images/chainDual.png",ease:Linear.easeIn},"-=0.1")
     //remove charms
     .to(".ms001_9r",1,{x:"-500%",zIndex:20,autoAlpha:0,ease:Power1.easeIn})
     .to(".ms001_ag",1,{x:"-500%",zIndex:20,autoAlpha:0,ease:Power1.easeIn})
