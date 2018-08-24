80320fec0b08d9bcf16f0ed998cf73c972d4503e
sayedfullah <42635601+sayedfullah@users.noreply.github.com>
2018-08-24 13:01:23 +0200

    Update mygsap.js

--- mygsap.js
+++ mygsap.js
@@ -1,3 +1,4 @@
+var bucky = document.document.getElementsByClassName('chn');
 var myScene = new TimelineMax({repeat:-1,repeatDelay:4,yoyo:false});
 myScene
     .to(".start",1,{x:"-2%",ease:Linear.easeOut})
@@ -18,7 +19,7 @@ myScene
     .to(".start",1,{x:"8%",ease:Linear.easeOut,zIndex:18},"-=0.5")
     .to(".end",1,{x:"-6.5%",ease:Linear.easeOut},"-=0.5")
     //swap out start chain
-    .to(".start",0.1,{css:{src:'https://s8.postimg.cc/esh0uemv9/chain_Dual.png'}},"-=0.1")
+    .to(".bucky[0]",0.5,{src:'/design/themes/GiddyStore/media/animation/01/res/images/chainDual.png'},"-=0.5")
     //remove charms
     .to(".ms001_9r",1,{x:"-500%",zIndex:20,autoAlpha:0,ease:Power1.easeIn})
     .to(".ms001_ag",1,{x:"-500%",zIndex:20,autoAlpha:0,ease:Power1.easeIn})
